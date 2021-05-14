cb1d387ab633319528246f0fd00ee67fac4e395e
LOKESH <lokeshkumarp@helixtechsolutions.com>
2021-05-14 10:30:48 +0530

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by apple on 14/05/21.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
